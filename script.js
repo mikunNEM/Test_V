c68161365180dff62c50a914e0b5df747d0bce8f
mikun <45915246+mikunNEM@users.noreply.github.com>
2022-12-04 20:08:21 +0900

    Update script.js

--- script.js
+++ script.js
@@ -208,7 +208,7 @@ const searchCriteria = {
   group: symbol.TransactionGroup.Confirmed,
   address,
   pageNumber: 1,
-  pageSize: 10,
+  pageSize: 3,
   order: symbol.Order.Desc,
   embedded: true,
 };
