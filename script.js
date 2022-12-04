e54043d08cc06f5dfcc53a769facd56ba671aa38
mikun <45915246+mikunNEM@users.noreply.github.com>
2022-12-04 18:36:55 +0900

    Update script.js

--- script.js
+++ script.js
@@ -357,7 +357,7 @@ transactionHttp
                                                   
                          
             dom_tx.appendChild(dom_message);                   // dom_message をdom_txに追加              
-            dom_tx.appendChild(document.createElement('hr'));  // 水平線を引く
+            dom_tx.appendChild(document.createElement('hr1'));  // 水平線を引く
             dom_txInfo.appendChild(dom_tx);                    // トランザクション情報を追加
     }    //    tx をループ処理
   })
