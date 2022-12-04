8b4de572f13a642d437646e3e7690f1da7857ab8
mikun <45915246+mikunNEM@users.noreply.github.com>
2022-12-04 22:46:30 +0900

    Update script.js

--- script.js
+++ script.js
@@ -301,28 +301,31 @@ transactionHttp
                (async() => {
                   let mosaicNames = await nsRepo.getMosaicsNames([new symbol.MosaicId(tx.mosaics[i].id.id.toHex())]).toPromise(); // Namespaceの情報を取得する
           
-                    console.log(mosaicNames);
+                    //console.log(mosaicNames);
                   mosaicInfo = await mosaicHttp.getMosaic(tx.mosaics[i].id.id).toPromise();// 可分性の情報を取得する      
                   
                   let div = mosaicInfo.divisibility; // 可分性
-                setTimeout(() => {  
+                 
                  if(tx.signer.address.address === address.address) {  // signerとウォレットのアドレスが同じかどうかで絵文字の表示と色を変える
                      if ([mosaicNames][0][0].names.length !==0){  // ネームスペースがある場合
                          dom_mosaic.innerHTML = `<font color="#FF0000">Mosaic :　<big><strong>${[mosaicNames][0][0].names[0].name}</strong></big>　<small>(${tx.mosaics[i].id.id.toHex()})</small></font>`; 
+                         console.log([mosaicNames][0][0].names[0].name)///////////////////////////////
                      }else{   　　　　　　　　　　　　　　　　　　　　　 //　ネームスペースがない場合
                          dom_mosaic.innerHTML = `<font color="#FF0000">Mosaic :　<small>(${tx.mosaics[i].id.id.toHex()})</small></font>`;
+                          console.log("Namespace無し",tx.mosaics[i].id.id.toHex())////////////////////////////////////
                      }    
                          dom_amount.innerHTML = `<font color="#FF0000" size="+1">💁‍♀️➡️💰 :<i><big><strong> ${(parseInt(tx.mosaics[i].amount.toHex(), 16)/(10**div)).toLocaleString(undefined, { maximumFractionDigits: 6 })} </big></strong><i></font>`;     // 　数量               
                  }else {                                         //  signer とウォレットアドレスが違う場合
                      if ([mosaicNames][0][0].names.length !==0){ // ネームスペースがある場合
+                         console.log([mosaicNames][0][0].names[0].name)/////////////////////////////// 
                          dom_mosaic.innerHTML = `<font color="#008000">Mosaic :　<big><strong>${[mosaicNames][0][0].names[0].name}</strong></big>　<small>(${tx.mosaics[i].id.id.toHex()})</small></font>`; 
                      }else{ 　　　　　　　　　　　　　　　　　　　　　  // ネームスペースがない場合
                          dom_mosaic.innerHTML = `<font color="#FF0000">Mosaic :　<small>(${tx.mosaics[i].id.id.toHex()})</small></font>`;
+                          console.log("Namespace無し",tx.mosaics[i].id.id.toHex())////////////////////////////////////
                      }
                          dom_amount.innerHTML = `<font color="#008000" size="+1">💰➡️😊 :<i><big><strong> ${(parseInt(tx.mosaics[i].amount.toHex(), 16)/(10**div)).toLocaleString(undefined, { maximumFractionDigits: 6 })} </big></strong><i></font>`;     // 　数量            
                  }
-              
-                }, 1000) // setTimeout    
+                
                })(); // async()
             }else { //モザイクが空の場合
                if(tx.signer.address.address === address.address) {  // 送信アドレスとウォレットのアドレスが同じかどうかで絵文字の表示と色を変える
