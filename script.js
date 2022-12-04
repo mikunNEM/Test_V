60fa757744005d5d8200a9b4fd3f7111660d978b
mikun <45915246+mikunNEM@users.noreply.github.com>
2022-12-04 17:38:52 +0900

    Update script.js

--- script.js
+++ script.js
@@ -336,6 +336,9 @@ transactionHttp
                 dom_tx.appendChild(dom_mosaic);                    // dom_mosaic をdom_txに追加 
                 dom_tx.appendChild(dom_amount);                    // dom_amount をdom_txに追加
                
+                if (i !== 0){
+                   dom_tx.appendChild(document.createElement('hr2'));  // 水平線を引く
+                }
                
                console.log("i=",i);
            }  //モザイクの数だけ繰り返す
