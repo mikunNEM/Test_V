0d639306fb1ea31faad6d1aca0c3c4124602e9da
mikun <45915246+mikunNEM@users.noreply.github.com>
2022-12-05 23:04:12 +0900

    Update script.js

--- script.js
+++ script.js
@@ -336,8 +336,8 @@ transactionHttp
                          dom_amount.innerHTML = `<font color="#008000" size="+1">💰➡️😊 :<i><big><strong> ${(parseInt(tx.mosaics[i].amount.toHex(), 16)/(10**div)).toLocaleString(undefined, { maximumFractionDigits: 6 })} </big></strong><i></font>`;     // 　数量                                 
                  }
                 
-                        console.log("モザイクネーム",[mosaicNames][0][0].names[0].name)/////////////////////////////// 
-                        console.log("ネーム無し, ID のみ",tx.mosaics[i].id.id.toHex())////////////////////////////////////
+                        console.log("モザイクネーム",[mosaicNames][0][0].names[0].name);  /////////////////////////////// 
+                        console.log("ネーム無し, ID のみ",tx.mosaics[i].id.id.toHex());  ////////////////////////////////////
                     
                })(); // async()
             }else{   //モザイクが空の場合
