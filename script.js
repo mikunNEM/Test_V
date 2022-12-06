e164b82c55f0afab06edb2915e4f89fb19b99ed8
mikun <45915246+mikunNEM@users.noreply.github.com>
2022-12-07 08:48:51 +0900

    Update script.js

--- script.js
+++ script.js
@@ -315,7 +315,7 @@ transactionHttp
                      }else{   　　　　　　　　　　　　　　　　　　　　　 //　ネームスペースがない場合
                          dom_mosaic.innerHTML = `<font color="#FF0000">Mosaic :　<strong>${tx.mosaics[i].id.id.toHex()}</strong></font>`;
                      }    
-                         dom_amount.innerHTML = `<font color="#FF0000" size="+1">💁‍♀️➡️💰 :<i><big><strong> ${(parseInt(tx.mosaics[i].amount.toHex(), 16)/(10**div)).toLocaleString(undefined, { maximumFractionDigits: 6 })} </big></strong><i></font>`;     // 　数量               
+                         dom_amount.innerHTML = `<font color="#FF0000" size="+1">💁‍♀️➡️💰 :　<i><big><strong> ${(parseInt(tx.mosaics[i].amount.toHex(), 16)/(10**div)).toLocaleString(undefined, { maximumFractionDigits: 6 })} </big></strong><i></font>`;     // 　数量               
                  }else{                                         //  signer とウォレットアドレスが違う場合
                       
                      if ([mosaicNames][0][0].names.length !==0){ // ネームスペースがある場合                         
@@ -323,7 +323,7 @@ transactionHttp
                      }else{ 　　　　　　　　　　　　　　　　　　　　　  // ネームスペースがない場合
                          dom_mosaic.innerHTML = `<font color="#FF0000">Mosaic :　<strong>${tx.mosaics[i].id.id.toHex()}</strong></font>`;                        
                      }
-                         dom_amount.innerHTML = `<font color="#008000" size="+1">💰➡️😊 :<i><big><strong> ${(parseInt(tx.mosaics[i].amount.toHex(), 16)/(10**div)).toLocaleString(undefined, { maximumFractionDigits: 6 })} </big></strong><i></font>`;     // 　数量                                 
+                         dom_amount.innerHTML = `<font color="#008000" size="+1">💰➡️😊 :　<i><big><strong> ${(parseInt(tx.mosaics[i].amount.toHex(), 16)/(10**div)).toLocaleString(undefined, { maximumFractionDigits: 6 })} </big></strong><i></font>`;     // 　数量                                 
                  }
                     
                })(); // async()
