91fe36bb46ba3ed848f31b59bbfc4d49145ec422
mikun <45915246+mikunNEM@users.noreply.github.com>
2022-12-07 15:13:50 +0900

    Update script.js

--- script.js
+++ script.js
@@ -298,16 +298,16 @@ transactionHttp
                const dom_amount = document.createElement('div');
                
                console.log("i=",i); 
+          (async() => {
             if (tx.mosaics.length !== 0){ //モザイクが空でない(モザイク有りの場合)                 
-               (async() => {
+               
                   let mosaicNames = await nsRepo.getMosaicsNames([new symbol.MosaicId(tx.mosaics[i].id.id.toHex())]).toPromise(); // Namespaceの情報を取得する
           
                     //console.log(mosaicNames);
                   mosaicInfo = await mosaicHttp.getMosaic(tx.mosaics[i].id.id).toPromise();// 可分性の情報を取得する      
                   
                   let div = mosaicInfo.divisibility; // 可分性
-                 
-                   
+                              
                  if(tx.signer.address.address === address.address) {  // signerとウォレットのアドレスが同じかどうかで絵文字の表示と色を変える
                       
                      if ([mosaicNames][0][0].names.length !==0){  // ネームスペースがある場合
@@ -326,7 +326,7 @@ transactionHttp
                          dom_amount.innerHTML = `<font color="#008000" size="+1">💰➡️😊 :　<i><big><strong> ${(parseInt(tx.mosaics[i].amount.toHex(), 16)/(10**div)).toLocaleString(undefined, { maximumFractionDigits: 6 })} </big></strong><i></font>`;     // 　数量                                 
                  }
                     
-               })(); // async()
+               
             }else{   //モザイクが空の場合
                  console.log("モザイクが空")
                    if(tx.signer.address.address === address.address) {  // 送信アドレスとウォレットのアドレスが同じかどうかで絵文字の表示と色を変える
@@ -337,7 +337,9 @@ transactionHttp
                        dom_amount.innerHTML = `<font color="#008000">💰➡️😊 : </font>`;     // 　数量               
                    }   
              }
-                     
+               
+           })(); // async()
+             
                 dom_tx.appendChild(dom_mosaic);                    // dom_mosaic をdom_txに追加 
                 dom_tx.appendChild(dom_amount);                    // dom_amount をdom_txに追加
                     
