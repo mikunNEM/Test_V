76ce96dc7471f63e16524eb11c6ca56efac9b92f
mikun <45915246+mikunNEM@users.noreply.github.com>
2022-12-20 00:56:36 +0900

    Update script.js

--- script.js
+++ script.js
@@ -342,7 +342,7 @@ txRepo
              
              if (tx.message.type === 1){
                  dom_enc.innerHTML = `<font color="#ff00ff"><strong></br><ul class="decryption"><li>暗号化メッセージ</li>
-		 <li><input type="button" value="復号化" onclick="OnButtonDecryption(tx,t);" class="button-decrypted"/></li></ul></strong></font>`;     // 暗号化メッセージの場合
+		 <li><input type="button" value="復号化" onclick="OnButtonDecryption(txs,t);" class="button-decrypted"/></li></ul></strong></font>`;     // 暗号化メッセージの場合
 		     
                  dom_tx.appendChild(dom_enc);
               
