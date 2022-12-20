238aecabf53394d1dae783f70b3c21c417805294
mikun <45915246+mikunNEM@users.noreply.github.com>
2022-12-20 20:17:34 +0900

    Update script.js

--- script.js
+++ script.js
@@ -347,13 +347,16 @@ txRepo
 		     
                  dom_tx.appendChild(dom_enc);
 		     
-		 if(tx.recipientAddress.address === tx.signer.address) {    // 送信先アドレスと、送信元アドレスが同じ場合
+		 if(tx.recipientAddress.address === tx.signer.address.address) {    // 送信先アドレスと、送信元アドレスが同じ場合
 		    　　PubKey = window.SSS.activePublicKey;
 		 }else{       // 送信先アドレスと、ウォレットアドレスが異なる場合
-			 
-			 
-			 const alice = sym.Address.createFromRawAddress(tx.recipientAddress.address);   //アドレスクラスの生成
-             　　　　　　　
+			 if (tx.signer.address.address === address.address){
+				 const alice = sym.Address.createFromRawAddress(tx.recipientAddress.address);   //アドレスクラスの生成
+             　　　　　　　}else
+			    if (tx.recipientAddress.address === address.address){
+			       const alice = sym.Address.createFromRawAddress(tx.signer.address.address);   //アドレスクラスの生成
+			    }
+				    
 			 accountInfo = await accountRepo.getAccountInfo(alice).toPromise();  //　送信先アドレスの公開鍵を取得する
              
            　　　　　　　   PubKey = accountInfo.publicKey;
