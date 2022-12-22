1a29d68ad075aca8ea36f1fcdb43173684b9f679
mikun <45915246+mikunNEM@users.noreply.github.com>
2022-12-22 15:44:04 +0900

    Update script.js

--- script.js
+++ script.js
@@ -386,7 +386,7 @@ txRepo
                    })
                   }, 3000). */		     
               
-                 dom_message.innerHTML = `<font color="#ff00ff">< Encrypted Message ></font><font color="#4169e1"></br><input type="button" id="enc_message" value="${tx.message.payload}" onclick="Onclick_Decryption(); class="button-decrypted""/></div></font>`;     // 　メッセージ    
+                 dom_message.innerHTML = `<font color="#ff00ff">< Encrypted Message ></font><font color="#4169e1"></br><input type="button" id="enc_message" value="${tx.message.payload}" onclick="Onclick_Decryption();" class="button-decrypted"/></div></font>`;     // 　メッセージ    
             }else{          // 平文の場合
                  dom_message.innerHTML = `<font color="#4169e1"></br>< Message ></br>${tx.message.payload}</font>`;     // 　メッセージ  
             }
