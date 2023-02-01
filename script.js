fd7a509eb81de860f022dead4254bb6ea519d8b5
mikun <45915246+mikunNEM@users.noreply.github.com>
2023-02-01 20:19:53 +0900

    Update script.js

--- script.js
+++ script.js
@@ -400,6 +400,7 @@ txRepo
               (async() => { 	  
                      const aggTx = await txRepo.getTransactionsById([tx.transactionInfo.hash],sym.TransactionGroup.Confirmed).toPromise();
 	             console.log(aggTx[0].innerTransactions[0].message.payload);
+		     console.log("aggTx.size",aggTx.length);
 		     dom_message.innerHTML = `<font color="#4169e1"></br>< Message aggTx[1]></br>${aggTx[0].innerTransactions[0].message.payload}</font>`;     // 　メッセージ              
 	      })();
 	  }	    
