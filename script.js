97af6d238a6c19137fe13ce676b4d4128cbb0ab8
mikun <45915246+mikunNEM@users.noreply.github.com>
2023-02-04 00:28:59 +0900

    Update script.js

--- script.js
+++ script.js
@@ -292,7 +292,7 @@ txRepo
                (async() => {
 	             let namespaceNames = await nsRepo.getNamespacesNames([sym.NamespaceId.createFromEncoded(tx.recipientAddress.id.toHex())]).toPromise();
 		      console.log("namespaceNames=",namespaceNames); 
-		     dom_recipient_address.innerHTML = `<font color="#2f4f4f">To :   ${namespaceNames}</font>`; //  文字列の結合　   宛先
+		     dom_recipient_address.innerHTML = `<font color="#2f4f4f">To :   ${[namespaceNames][0].name}</font>`; //  文字列の結合　   宛先
                 })(); // async() 
 	    }else{   
                    dom_recipient_address.innerHTML = `<font color="#2f4f4f">To :   ${tx.recipientAddress.address}</font>`; //  文字列の結合　   宛先
